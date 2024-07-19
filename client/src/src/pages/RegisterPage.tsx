ef472c112df72f4bf8ca16d8e83a963f4d150b70
rchagolla <123438125+rchagolla@users.noreply.github.com>
2024-07-18 18:25:07 -0700

    register inputs are required now.

--- client/src/src/pages/RegisterPage.tsx
+++ client/src/src/pages/RegisterPage.tsx
@@ -4,6 +4,49 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function RegisterPage() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+    bio: "",
+    language: "en",
+  });
+  const [errors, setErrors] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+    bio: "",
+    language: "",
+  });
+
+  const setField = (field:string, value:string) => {
+    setFormData({
+      ...formData,
+      [field]: value
+    })
+  };
+
+  const findErrors = () => {
+    const {username, password, confirmPassword, bio, language} = formData;
+    const [newErrors, setNewErrors] = useState({});
+    if (!username || username === '') {
+      setNewErrors({
+        ...newErrors,
+        ['username']: 'Username can\'t be empty!'
+      })
+    }
+
+    return newErrors;
+  };
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    console.log(formData.username);
+    console.log(formData.password);
+    console.log(formData.confirmPassword);
+    console.log(formData.bio);
+    console.log(formData.language);
+  };
 
   return (
     <Container>
@@ -12,51 +55,69 @@ function RegisterPage() {
 
         <Card.Body>
           <FormContent>
-            <Form>
-              {/* Email Input */}
-              <Form.Group className='mb-3'>
+            <Form onSubmit={handleSubmit}>
+              {/* Username Input */}
+              <Form.Group className='mb-3' controlId='username'>
                 <FloatingLabel controlId="floatingInput" label="Username">
-                  <Form.Control type="text" placeholder="Username" />
+                  <Form.Control 
+                    type="text" 
+                    placeholder="Username" 
+                    name="username"
+                    onChange={e => setField('username', e.target.value)}
+                    isInvalid={!!errors.username}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please enter a valid username (alphanumeric
+                    characters only).
+                  </Form.Control.Feedback>
                 </FloatingLabel>
               </Form.Group>
 
               {/* Password Input */}
-              <Form.Group className='mb-3'>
+              <Form.Group className='mb-3' controlId='password'>
                 <FloatingLabel controlId="floatingInput" label="Password">
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control 
+                    type="password" 
+                    placeholder="Password"
+                    name="password"
+                    onChange={e => setField('password', e.target.value)}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Your password must be 8-20 characters long, contain a letter, number, special character,
+                    and must not contain spaces, or emoji.
+                  </Form.Control.Feedback>
                 </FloatingLabel>
-                <Form.Text id="passwordHelpBlock" muted>
-                  Your password must be 8-20 characters long, contain letters and numbers,
-                  and must not contain spaces, special characters, or emoji.
-                </Form.Text>
               </Form.Group>
 
               {/* Password Re-type */}
               <Form.Group className='mb-3'>
                 <FloatingLabel controlId="floatingInput" label="Re-type Password">
-                  <Form.Control type="password" placeholder="Re-type Password" />
-                </FloatingLabel>
-              </Form.Group>
-
-              {/* Bio Input */}
-              <Form.Group className='mb-3'>
-                <FloatingLabel controlId="floatingInput" label="Bio">
-                  <Form.Control as="textarea" placeholder="Bio" />
+                  <Form.Control 
+                  type="password"
+                  placeholder="Re-type Password"
+                  name="passwordConfirm"
+                  onChange={e => setField('confirmPassword', e.target.value)}
+                  required
+                />
                 </FloatingLabel>
               </Form.Group>
 
               {/* Language Selection */}
-              <Form.Group>
+              <Form.Group className='mb-3'>
                 <SelectGroup>
-                  <FormSelect>
-                    <option value='en'> English </option>
-                    <option value='es'> Spanish </option>
-                    <option value='fr'> French </option>
-                  </FormSelect>
+                  <FloatingLabel controlId="floatingSelect" label="Preferred language"> 
+                    <FormSelect name='language' onChange={e => setField('language', e.target.value)}>
+                      <option value='en'> English </option>
+                      <option value='es'> Spanish </option>
+                      <option value='fr'> French </option>
+                    </FormSelect>
+                  </FloatingLabel>
                 </SelectGroup>
               </Form.Group>
 
-              <Button> Register </Button>
+              <Button type='submit'> Register </Button>
 
               <p className='fs-6'> Already have an account? <a href='/login'>Log in</a> </p>
 
@@ -81,7 +142,8 @@ const FormContent = styled('div')`
 `;
 
 const SelectGroup = styled('div')`
-  padding-right: 65%;
+  padding-right: 35%;
+  padding-left: 35%;
 `;
 
 export default RegisterPage
