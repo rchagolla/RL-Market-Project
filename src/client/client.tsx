69197c129a7936d78958ed03ba9387c468a2f87c
rchagolla <123438125+rchagolla@users.noreply.github.com>
2024-07-23 13:43:08 -0700

    bootstrap is handled by webpack.

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOMClient from "react-dom/client";
 import nullthrows from "nullthrows";
 import  App  from "./components/App";
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function main() {
   const domRoot = document.getElementById("root");
