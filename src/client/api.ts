b8eea250f866fe23ce40c84326cced4d8af3fd76
rchagolla <123438125+rchagolla@users.noreply.github.com>
2024-07-25 15:41:02 -0700

    stops user from getting to home page with url.

--- src/client/api.ts
+++ src/client/api.ts
@@ -1,4 +1,4 @@
 import { rpcClient } from "typed-rpc";
 import { APIService } from "../server/server";
 
-export const api = rpcClient<APIService>("http://localhost:5000/rpc");
+export const api = rpcClient<APIService>("/rpc");
