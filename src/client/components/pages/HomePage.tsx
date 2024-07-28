dcac39b84168c989800495217a95214c7c80af67
rchagolla <123438125+rchagolla@users.noreply.github.com>
2024-07-27 20:58:32 -0700

    displays all Rocket league items.

--- src/client/components/pages/HomePage.tsx
+++ src/client/components/pages/HomePage.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
-import { useCurrentUser } from '../hooks/useCurrentUser'
+import React, {useState} from 'react';
+import { useCurrentUser } from '../hooks/useCurrentUser';
+import { products } from '@rocketleagueapi/items';
+import { Card, ListGroup, Container } from 'react-bootstrap';
+
+interface Product {
+  name: string;
+  blueprint: boolean;
+  special: number;
+  paintable: boolean;
+  currency: boolean;
+  quality: number;
+  slot: number;
+  tradable: boolean;
+  tradeIn: boolean;
+};
+
+interface ProductCollection {
+  [key: string]: Product;
+};
 
 function HomePage() {
   const user = useCurrentUser();
+  const prods: ProductCollection = products as ProductCollection;
 
   return (
-    <div>HomePage</div>
+    <Container className='p-3'>
+      {Object.entries(prods).map(([key, value]) => (
+        <Card style={{ width: '18rem' }} key={key}>
+          <Card.Img variant="top" src="public/img/comingSoon.jpeg" />
+          <Card.Body>
+            <Card.Title>{value.name}</Card.Title>
+          </Card.Body>
+        </Card>
+      ))}
+
+      {/* <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="./public/img/comingSoon.jpeg" />
+        <Card.Body>
+          <Card.Title>pending</Card.Title>
+        </Card.Body>
+      </Card> */}
+    </Container>
   )
 }
 
