913bd6125097eecc1d17564c93428db270678c45
rchagolla <123438125+rchagolla@users.noreply.github.com>
2024-07-30 16:13:26 -0700

    setup basic account details card.

--- src/client/components/pages/ProfilePage.tsx
+++ src/client/components/pages/ProfilePage.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Button } from 'react-bootstrap';
+import { Button, Container, Tab, Nav, Col, Row } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { useCurrentUser } from '../hooks/useCurrentUser';
 import { useAuthentication } from '../hooks/useAuthentication';
+import AccountDetails from '../profilePage/AccountDetails';
 
 function ProfilePage() {
   const user = useCurrentUser();
@@ -20,11 +21,31 @@ function ProfilePage() {
   }
 
   return (
-    <div>
-      ProfilePage
-      <br></br>
-      <Button onClick={logout}>Logout</Button>
-    </div>
+    <Container fluid className='p-4 text-center'>
+      <h1>Welcome, {user?.username}!</h1>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <Row>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="first">Account Details</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">My Orders</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">
+                <AccountDetails></AccountDetails>
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </Container>
   )
 }
 
